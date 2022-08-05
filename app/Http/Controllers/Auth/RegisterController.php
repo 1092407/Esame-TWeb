795b5dac0e659488652b62028c75e36d2874a935
giorg <s1092407@studenti.univpm.it>
2022-08-05 22:46:13 +0200

    regoster

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
@@ -21,14 +22,14 @@ class RegisterController extends Controller
     |
     */
 
-    use RegistersUsers; //Trait classe predefinita che mette a disposizone una serie di metodi già predefiniti
+    use RegistersUsers;
 
     /**
      * Where to redirect users after registration.
      *
      * @var string
      */
-    protected $redirectTo = '/'; //proprietà predefinita che specifica la rotta a cui viene reindirizzato l'utente a fine registrazione
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -37,13 +38,12 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest'); //metodo che ci consente di attivare i gate 
+        $this->middleware('guest');
     }
 
     /**
      * Get a validator for an incoming registration request.
-     * Sovrascriviamo uno dei metodi predefinifiti del trait RegisterUsers, questo metodo permette di
-     * definire delle regole di validazione direttamente nel controller invece che definirle nella cartella Request
+     *
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
@@ -58,22 +58,21 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'unique:users', 'regex:/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/'],
             'username' => ['required', 'string', 'min:8', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'cellulare' => ['required', 'string', 'min:10', 'max:10', 'unique:users'],
-            'livello' => ['required', 'integer'],
-            'descrizione' => ['sometimes','string','max:2500']
+            // penso che livello non vada messo perchè non è un campo che compare nella form di registrazione
+            //infatti voglio mettere che livello=utente di default per chi si registra tramite la viwe registrazione
+            'descrizione' => ['required ','string','max:2500']
         ]);
     }
 
     /**
      * Create a new user instance after a valid registration.
-     * Procedura per la creazione di utenti una volta che la procedura è stata validata,
-     * meccanismo che associa i dati della vista alle proprietà della classe Users che sarà mappato nella tupla della base di dati
+     *
      * @param  array  $data
      * @return \App\User
      */
     protected function create(array $data)
     {
-        if (request()->hasFile('foto_profilo')) {
+         if (request()->hasFile('foto_profilo')) {
             $image = request()->file('foto_profilo');
             $imageName = $image->getClientOriginalName();
             $destinationPath = public_path().'/img/foto_profilo';
@@ -90,8 +89,8 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'username' => $data['username'],
             'password' => Hash::make($data['password']),
-            'cellulare' => $data['cellulare'],
-            'livello' => $data['livello'],
+            // 'livello' == 'utente',             // lo voglio per default per chi si registra tramite questa pagina: l'admin infatti lo preregistro nel db con i seeds e i membri dello staff li registro tramite la sezione riservata dell'admin
+// non so se la sintassi corretta sia == 0 =>  DA CONTROLLARE
             'descrizione' => $data['descrizione']
         ]);
 
