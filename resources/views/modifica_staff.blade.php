a0859bdf42dc64093e8daced692db7b87f81d08c
giorg <s1092407@studenti.univpm.it>
2022-08-15 12:47:15 +0200

    adesso la modifica va quasi bene . bisogna sistemare la vista a livello di stile e che non mi obllighi a fare certe cose durante le modifiche

--- resources/views/modifica_staff.blade.php
+++ resources/views/modifica_staff.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.admin')
 @section('title', 'Modifica staff')
 
@@ -5,15 +6,35 @@
 @isset($staff)
 
 
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:300px">
 
-<div class="static w3-center">
-    <h2><b>ora modifichiamo lo  staff</b></h2>
-    <p>Utilizza questa form per registrare i membri del tuo staff .</p>
-    <hr>
-    <div class="container-contact">
-        <div class="wrap-contact1">
+  <!-- Header -->
+  <header id="portfolio">
+    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+    <div class="w3-container">
+      <h1 id="titolo" style="margin-bottom:0px;"><b>Gestione staff</b></h1>
+      <div class=" w3-bottombar w3-padding-16" style="margin-bottom:16px;">
+      </div>
+      @if (session('status'))
+      <div class="alert success">
+        {{ session('status') }}
+      </div>
+      @endif
+    </div>
+  </header>
 
- {{ Form::open(array('route' => ['staff.update',$staff->id ], 'method' => 'PUT', 'id'=>'modificastaff', 'class' => 'animate')) }}
+  <!-- First Photo Grid-->
+  <div style="padding-left: 20px; padding-right: 20px;">
+    <div class="col-sm-12">
+    <div class="w3-container" align='center'>
+    <div class="w3-modal-content w3-animate-zoom">
+        <div class="w3-container  w3-blue">
+            <h2>Qui modifichi i dati di un membro staff</h2>
+        </div>
+        <div class="w3-panel" style="padding-bottom:16px;">
+            <div align='center'>
+                {{ Form::open(array('route' => ['staff.update',$staff->id ], 'method' => 'PUT', 'id'=>'modificastaff', 'class' => 'animate')) }}
 
               {{ Form::label('name', 'Nome', ['class' => 'label-input-alloggio']) }}
                 {{ Form::text('name',$staff->name, ['class' => 'text-input-alloggio', 'id' => 'name']) }}
@@ -27,7 +48,7 @@
 
 
 
-                {{ Form::label('cognome', 'Cognome', ['class' => 'label-input']) }}
+                {{ Form::label('cognome', 'Cognome', ['class' => 'label-input-alloggio']) }}
                 {{ Form::text('cognome', $staff->cognome, ['class' => 'input', 'id' => 'surname']) }}
                 @if ($errors->first('cognome'))
                 <ul class="errors">
@@ -96,17 +117,11 @@
 
                 {{ Form::submit('Salva Modifica', ['class' => 'w3-button w3-right w3-blue' , 'style'=> "width:150px"]) }}
                 {{Form::close()}}
-
-
-
+            </div>
         </div>
     </div>
-
 </div>
-
-
-
-
+//finire a sistemare
 
 @endisset
 @endsection
