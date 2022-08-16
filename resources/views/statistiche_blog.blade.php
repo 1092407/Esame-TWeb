9071d0cb6e1d1ec67a34cf90d103f76e5f542593
giorg <s1092407@studenti.univpm.it>
2022-08-16 23:16:28 +0200

    statistiche relative a blog ok

---
+++ resources/views/statistiche_blog.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.admin')
+
+@section('title', 'statistiche blogs')
+
+@section('content')
+<div class="w3-container w3-padding-32" id="catalog" align="center">
+
+
+<div class="w3-main" style="margin-left:30px">
+
+
+
+  <!-- First Photo Grid-->
+  <div style="padding-left: 20px; padding-right: 20px;">
+    <div class="col-sm-12">
+      <table class="w3-table-all table-striped">
+        <thead>
+          <tr>
+            <td><b style="font-size:18px;">titolo</b></td>
+            <td><b style="font-size:18px;">descrizione</b></td>
+
+          </tr>
+        </thead>
+
+
+        <tbody>
+
+
+          @foreach($blogs as $blog)
+          <tr>
+            <td>{{$blog->titolo}}</td>
+            <td>{{$blog->descrizione}}</td>
+
+
+
+
+          @endforeach
+
+
+
+        </tbody>
+
+
+      </table>
+
+
+</div>
+
+@endsection
