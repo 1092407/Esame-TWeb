d6255b879710b0476cbe6a64f7e0a7e772d776d6
giorg <s1092407@studenti.univpm.it>
2022-09-19 22:13:14 +0200

    sistemato carattere jolly

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -6,6 +6,7 @@
 <div class="w3-container w3-padding-32" id="catalog" align="center">
     <h1 class="w3-border-bottom w3-border-light-grey w3-padding-16"><b>ECCOTI </b></h1>
     <h4> BENVUNUTO NELLA HOMEPAGE DELLA NOSTRA COMUNITA!</h4><br>
+     <h5>Corri a registrarti o se lo hai già fatto clicca su "Accedi" per effettuare il login </h5><br>
 
 
 
